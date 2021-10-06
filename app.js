586066bfcc15d969dd3c84992a6af201fe457c6d
whydn <whydin.id@gmail.com>
2021-10-07 02:23:38 +0700

    feat: add cors lib

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
